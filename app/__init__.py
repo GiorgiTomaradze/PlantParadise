ec19a1dcb53416fb0b8fb5e7949e90707a0cf7c9
GiorgiTomaradze <tomaradzegiorgi7@gmail.com>
2024-06-28 20:33:33 +0400

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,2 @@
+from extensions import app
+from routes import home,about,products,blogs,product,blog,add_product,add_blog,edit_product,delete_product,edit_blog,delete_blog,singup,login,logout,search
